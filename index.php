78cab5d560cc2f787598bd5db42747f38448cda5
Penagunda, Reanne <rpenagunda@yahoo.com>
2016-04-11 15:30:33 +0100

    Created starting template using Bootstrap

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include 'includes/header.php';?>
+<?php include 'includes/nav.php';?>
+
+<div class="container">
+
+      <div class="starter-template">
+        <h1>Bootstrap starter template</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+      </div>
+
+</div><!-- /.container -->
+
+<?php include 'includes/footer.php';?>
